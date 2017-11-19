4a32675760b8aad2fdd0dc53a6d427864faeb51c
1845314 <z.mitev@student.utwente.nl>
2017-11-19 17:23:37 +0100

    Move constants to config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Lists
+lowers = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 'v', 'w', 'x', 'y', 'z']
+uppers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+symbols = ['!', '@', '#', '$', '%', '*', '(',')', '_', '-']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+groups = [lowers, uppers, symbols, numbers]
+characters = lowers + uppers + symbols + numbers
+
+minimum = 5
